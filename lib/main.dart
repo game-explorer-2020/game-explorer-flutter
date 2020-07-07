a78e6dedd269634f09bf031fb6a9c3fe3cef7475
Junior <vilmarmendesjunior@hotmail.com>
2020-07-07 20:52:37 -0300

    style: add trailing commas

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,15 @@ class GameExplorer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Game Explorer',
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Game Explorer'),
-            ),
-            body: Center(
-              child: Text('Hello World'),
-            )));
+      title: 'Game Explorer',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Game Explorer'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
   }
 }
